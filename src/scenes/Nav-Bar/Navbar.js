71f69cdf153528b42096f716ba386a67b5a23a27
Elyes-Ferjani <officialef@hotmail.com>
2021-05-28 02:04:19 +0200

    Navbar Purchase Basket

--- src/scenes/Nav-Bar/Navbar.js
+++ src/scenes/Nav-Bar/Navbar.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import '../../theme/style/navbar.css'
-const Navbar = (props) =>{
-    return(
+const Navbar = (props) => {
+    const [url, setUrl] = useState('/')
+    const [classname, setClassname] = useState('logo-wrapper')
+    const [productsView, setProductsView] = useState(true)
+    const ShoppingCartStyle = { color: 'white', position: 'absolute', right: '20', top: '20', border: '2px solid black', borderStyle: 'dotted', borderRadius: '100px', fontSize: '30px', padding: '5px', cursor: 'pointer' }
+    let history = useHistory()
+    const toRoute = (endpoint) => {
+        setUrl(endpoint)
+        history.push(endpoint)
+    }
+    useEffect(() => {
+        let cname = window.location.href.split('/').includes('products') ? 'logo-wrapper-products' : 'logo-wrapper'
+        cname == 'logo-wrapper-products' ? setProductsView(true) : setProductsView(false)
+        setClassname(cname)
+    }, [url])
+    return (
         <div className='navbar-container'>
             <section>
-                <div className='logo-wrapper'>
+                <div className={classname}>
                     MyShake
                 </div>
             </section>
             <section className='navbar-list-container'>
                 <ul className='navbar-list'>
-                    <li>HOME</li>
-                    <li>PRODUCTS</li>
-                    <li>ABOUT US</li>
+                    <li onClick={() => toRoute('/home')}>HOME</li>
+                    <li onClick={() => toRoute('/products')}>PRODUCTS</li>
+                    <li onClick={() => toRoute('/about')}>ABOUT US</li>
                 </ul>
+                {productsView ?
+                    <AddShoppingCartIcon style={ShoppingCartStyle} /> :
+                    <ShoppingCartIcon style={ShoppingCartStyle} /> 
+                }
             </section>
         </div>
     )
