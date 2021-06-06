2800d9a8bdaad5e4336ed3248ad3148360381389
Elyes-Ferjani <officialef@hotmail.com>
2021-06-07 00:09:59 +0200

    Modifying And Adding to Product Info View

--- src/scenes/ProductInfo/ProductInfo.js
+++ src/scenes/ProductInfo/ProductInfo.js
@@ -1,33 +1,56 @@
 import react from 'react'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import Card from '../../components/Card'
+import ON from '../../assets/ON.png'
+import Scitec from '../../assets/Scitec.png'
+import Dymatize from '../../assets/Dymatize.png'
+import Universal from '../../assets/Universal.png'
 import '../../theme/style/product-info.css'
-const ProductInfo = () =>{
+const ProductInfo = () => {
     const params = useParams()
     console.log(params.product)
-    const buy = (item) =>{
-            let exist = localStorage.getItem('bucket')
-            let addToItems = () => {
-                let s = JSON.parse(exist)
-                s.push(params.product)
-                return [...new Set(s)]
-            }
-            let addNew = () => localStorage.setItem('bucket',JSON.stringify([params.product]))
-            return exist ? localStorage.setItem('bucket', JSON.stringify(addToItems())) : addNew()
+    const buy = (item) => {
+        let exist = localStorage.getItem('bucket')
+        let addToItems = () => {
+            let s = JSON.parse(exist)
+            s.push(params.product)
+            return [...new Set(s)]
+        }
+        let addNew = () => localStorage.setItem('bucket', JSON.stringify([params.product]))
+        return exist ? localStorage.setItem('bucket', JSON.stringify(addToItems())) : addNew()
     }
-    return(
+    return (
         <div className='one-product-info-container'>
-         <div>
-        <Card />
-        </div> 
-        <div>
-            <p>
-                
+            <div>
+                <Card />
+            </div>
+            <div>
+                <h2>About Protein</h2>
+                <p className='product-informations'>
+                    Protein powder is a popular nutritional supplement. Protein is an essential macronutrient that helps build muscle, repair tissue,
+                     and make enzymes and hormones. Using protein powder may also aid weight loss and help people tone their muscles
             </p>
-            <button className="buy-button" onClick={()=>buy()}>
-                Add To Card
-            </button>
-        </div>  
+                <center><button className="buy-button" onClick={() => buy()}>
+                    Add To Card
+            </button></center>
+            </div>
+            <div className='partners'>
+                <h1>OUR BRANDS</h1>
+                <div className='products-partners'>
+                    <div>
+                        <img src={ON} />
+                    </div>
+                    <div>
+                        <img src={Scitec} />
+                    </div>
+                    <div>
+                        <img src={Dymatize} />
+                    </div>
+                    <div>
+                        <img src={Universal} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
